ebda19c866c32846e075aad3673ee67d0882a81e
WardCast <killianward@gmail.com>
2022-05-01 16:04:33 +0200

    reverted previous commit

--- hallway_salutes.py
+++ hallway_salutes.py
@@ -1,5 +1,28 @@
 salute_pairs = []
 
+def trim(hall):
+    string_vers = ""
+    has_been_right = False
+    has_been_left = False
+    list_vers = list(hall)
+    for x, char in enumerate(list_vers):
+        if char == ">":
+            has_been_right = True
+        if char == "<" and not has_been_right:
+            list_vers.pop(x)
+            list_vers.insert(x, "-")
+                
+    for x, char in enumerate(list_vers[::-1]):
+        if char == "<":
+            has_been_left = True
+        if char == ">" and not has_been_left:
+            list_vers.pop(x)
+            list_vers.insert(x, "-")
+    
+    for i in list_vers:
+        string_vers += i
+    return string_vers
+
 def filter_salutes(salute_pairs):
     for i in salute_pairs:
         comparing_list = salute_pairs
@@ -10,19 +33,19 @@ def filter_salutes(salute_pairs):
     return salute_pairs
             
 def solution(s):
-    trimmed_hall = s.strip("-")
+    trimmed_hall = trim(s)
     for x, char in enumerate(trimmed_hall):
         if char == ">":
             chars_in_path = trimmed_hall[x+1:]
-            for char_in_path in chars_in_path:
-                if char_in_path == "<":
-                    salute_pair = [x, char_in_path]
+            for approching_char in chars_in_path:
+                if approching_char == "<":
+                    salute_pair = [x, approching_char]
                     salute_pairs.append(salute_pair)
         elif char == "<":
             chars_in_path = trimmed_hall[:x]
-            for char_in_path in chars_in_path:
-                if char_in_path == ">":
-                    salute_pair = [x, char_in_path]
+            for approching_char in chars_in_path:
+                if approching_char == ">":
+                    salute_pair = [x, approching_char]
                     salute_pairs.append(salute_pair)
     
     filtered_salute_pairs = filter_salutes(salute_pairs)
@@ -30,6 +53,4 @@ def solution(s):
     return num_of_salutes
     
             
-print(solution("-<><><<-"))           
-    
-        
+print(solution("-<><><<-"))      
